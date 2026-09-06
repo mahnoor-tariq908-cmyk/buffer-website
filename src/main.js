664b1bfa6b5d175698c3653e2d39a89276012a3b
NoorTariq2005 <noort8776@gmail.com>
2026-09-06 21:57:48 +0500

    Make Buffer responsive with hamburger menu

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
